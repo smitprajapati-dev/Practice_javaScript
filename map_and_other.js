9ba4efaf2fb380cd9c728b81fb1c22b1fd3a5521
smit prajapati <smitprajapati002@gmail.com>
2024-03-08 17:42:01 -0600

    half of DOM is over

--- map_and_other.js
+++ map_and_other.js
@@ -5,8 +5,13 @@ mp.set("lnastName", "Prajapati");
 mp.set("favouriteColor", "Blue");
 mp.set("favouriteSport", "MMA")
 
-for(const [key, value] of mp){ // in [] we can print kee of map, and also we can print value 
-    console.log(value);
-    console.log(key);
+// for(const [key, value] of mp){ // in [] we can print kee of map, and also we can print value 
+//     console.log(key, ':-', value);
+// }
+  // you can't apply this method to object to pritnt key and value, and to print the value or key you just .value or .key to get output
+const obj = {
+    name:'smit',
+    lastName:'Prajapati',
+    age:21,
 }
-  
+console.log(obj.keys());
