7f08eb769a60e6ca0e9efbbf1d864aa225274bac
smit prajapati <smitprajapati002@gmail.com>
2024-03-13 12:30:16 -0600

    BMI calculator complete

--- Projects/colorSwitcher/script.js
+++ Projects/colorSwitcher/script.js
@@ -1,26 +1,42 @@
 const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
-buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener("click" , function(e){
-        console.log(e);
-        console.log(e.target);
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id
-        }
-        
-        if(e.target.id === 'White'){
-            body.style.backgroundColor = e.target.id
-        }
-        
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id
-        }
-        
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id
-        }
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
 
-    });
-})
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "White":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        break;
+      default:
+        break;
+    }
+    // if(e.target.id === 'grey'){
+    //     body.style.backgroundColor = e.target.id
+    // }
+
+    // if(e.target.id === 'White'){
+    //     body.style.backgroundColor = e.target.id
+    // }
+
+    // if(e.target.id === 'blue'){
+    //     body.style.backgroundColor = e.target.id
+    // }
+
+    // if(e.target.id === 'yellow'){
+    //     body.style.backgroundColor = e.target.id
+    // }
+  });
+});
