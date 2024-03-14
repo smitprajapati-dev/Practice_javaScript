968e8c53c77010ce0d02e0a4e1c20dfd18c0ca69
smit prajapati <smitprajapati002@gmail.com>
2024-03-13 18:44:10 -0600

    Guess the number UI complte

--- Projects/DigitalClock/script.js
+++ Projects/DigitalClock/script.js
@@ -3,5 +3,5 @@ const clock = document.getElementById("clock");
 setInterval(function () {
   let date = new Date();
 //   console.log(date.toLocaleTimeString);
-    clock.innerHTML = date.toLocaleTimeString();
+    clock.innerHTML = date.toLocaleTimeString(); 
 }, 1000);
